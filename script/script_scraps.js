29a6ed400ece602077ee141d48371b96d57e9585
eugenepash <jekpash@gmail.com>
2020-11-15 21:46:59 +0300

    new

---
+++ script/script_scraps.js
@@ -0,0 +1,6 @@
+const pz = document.querySelector(".pz");
+const pu = document.querySelector(".pu");
+const red_scrap = document.querySelector(".red_scrap");
+const red_scrap = document.querySelector(".black_scrap");
+
+                                    
