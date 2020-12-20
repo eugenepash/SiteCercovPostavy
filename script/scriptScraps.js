3f7a32819cab0d8be6136e71dec37f3dc4be1f9b
eugenepash <jekpash@gmail.com>
2020-12-20 17:24:23 +0300

    Скрипт записок и мелкие исправления кода
    1. Удалены лишние строки кода со всех страниц
    2. Написан скрипт записок
    3. переименован скрип записок (теперь scriptScraps.js)

---
+++ script/scriptScraps.js
@@ -0,0 +1,44 @@
+const redScrap = document.querySelector('.red_scrap');
+const blackScrap = document.querySelector('.black_scrap');
+
+const redScrapCheck = document.getElementById('redScrapCheck');
+const blackScrapCheck = document.getElementById('blackScrapCheck');
+
+const zdravieList = document.querySelector('.zdravieList');
+const panixidaList = document.querySelector('.panixidaList');
+
+const addnewnameBtnRed = document.querySelector('.addnewnameBtnRed');
+const addnewnameBtnBlack = document.querySelector('.addnewnameBtnBlack');
+let cnameRed = document.querySelector('.cnameRed');
+let cnameBlack = document.querySelector('.cnameBlack');
+let addRed = document.querySelector('.addRed');
+let addBlack = document.querySelector('.addBlack');
+
+
+
+
+
+redScrapCheck.onclick = function(){
+    redScrap.style.display = 'flex';
+    blackScrap.style.display = 'none';
+    zdravieList.style.display = 'block';
+    panixidaList.style.display = 'none';
+    
+}
+
+blackScrapCheck.onclick = function(){
+    redScrap.style.display = 'none';
+    blackScrap.style.display = 'flex';
+    zdravieList.style.display = 'none';
+    panixidaList.style.display = 'block';
+}
+
+
+addnewnameBtnRed.onclick = function(){
+    var divClone = cnameRed.cloneNode(true);
+    addRed.appendChild(divClone);
+}
+addnewnameBtnBlack.onclick = function(){
+    var divClone = cnameBlack.cloneNode(true);
+    addBlack.appendChild(divClone);
+}
